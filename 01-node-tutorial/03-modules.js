247546fa4f6b01eac7943080eee5eed46c09325a
hwangblood <hwangblood@gmail.com>
2023-02-20 19:57:20 +0800

    AlterNative Syntax

--- 01-node-tutorial/03-modules.js
+++ 01-node-tutorial/03-modules.js
@@ -3,6 +3,9 @@
 
 const names = require("./04-names");
 const sayHi = require("./05-utils");
+const data = require("./06-alternative-flavor");
+
+console.log(data);
 
 sayHi("hwangblood");
 sayHi(names.john);
