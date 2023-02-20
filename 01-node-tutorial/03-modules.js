b2e5224ee1589754c563e5492fb00d7701ef10d5
hwangblood <hwangblood@gmail.com>
2023-02-20 20:04:13 +0800

    Mind Grenade

--- 01-node-tutorial/03-modules.js
+++ 01-node-tutorial/03-modules.js
@@ -5,7 +5,7 @@ const names = require("./04-names");
 const sayHi = require("./05-utils");
 const data = require("./06-alternative-flavor");
 
-console.log(data);
+require("./07-mind-grenade");
 
 sayHi("hwangblood");
 sayHi(names.john);
