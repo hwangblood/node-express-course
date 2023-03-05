06e9a41706f29eeaf4a8e655963549335516d779
hwangblood <hwangblood@gmail.com>
2023-03-05 13:37:34 +0800

    Streams - Additional Info

--- 01-node-tutorial/18-streams.js
+++ 01-node-tutorial/18-streams.js
@@ -1,7 +1,14 @@
 const { createReadStream } = require("fs");
 
-const stream = createReadStream("./content/big.txt");
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream("./content/big.txt");
+// const stream = createReadStream("./content/big.txt", { highWaterMark: 90000 });
+const stream = createReadStream("./content/big.txt", { encoding: "utf8" });
+// const stream = createReadStream("./content/file-not-exist.txt");
 
 stream.on("data", (result) => {
   console.log(result);
 });
+stream.on("error", (err) => console.log(`Error: ${err}`));
